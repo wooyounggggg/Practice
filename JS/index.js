203deb29c34c7cccec1fef21f39efde89993b650
souljit2 <souljit2@gmail.com>
2019-05-25 07:58:52 -0400

    Modifying the DOM with JS

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,11 @@
 // document:Object
 const title=document.getElementById('title');
+const title2=document.querySelector('#title');
+console.log(`title1==title2 : ${title==title2}`);
 // console.log(title);
 title.innerHTML="Hi! From JS"
+title.style.color='red';
+document.title="After change"
+// search title's object info.
+// console.dir(title);
+
