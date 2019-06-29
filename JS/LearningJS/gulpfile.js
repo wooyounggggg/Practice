34735b91adf456034f9d744fb46543b08da59370
souljit2 <souljit2@gmail.com>
2019-06-29 04:42:16 -0400

    CREATE:Learning JS

---
+++ JS/LearningJS/gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+
+gulp.task('default',async function(){
+		gulp.src("es6/**/*.js")
+			.pipe(babel())
+			.pipe(gulp.dest("dist"));
+		gulp.src("public/es6/**/*.js")
+			.pipe(babel())
+			.pipe(gulp.dest("public/dist"));
+		})
+
