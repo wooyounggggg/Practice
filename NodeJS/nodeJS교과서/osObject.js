c08c5a8ab66b1058116cf20684c220e1bed2d179
Wooyoung <souljit2@gmail.com>
2019-07-27 00:52:38 +0900

    osObject

---
+++ NodeJS/nodeJS교과서/osObject.js
@@ -0,0 +1,12 @@
+const os = require('os');
+console.log(os.arch);
+console.log(os.platform());
+console.log(os.type());
+console.log(os.uptime());
+console.log(os.hostname());
+console.log(os.release());
+console.log(os.homedir());
+console.log(os.tmpdir());
+console.log(os.freemem());
+console.log(os.totalmem());
+console.log(os.cpus());
