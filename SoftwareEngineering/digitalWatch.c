e104ba76bf18041c5545d99e1d33a29b02063f00
Wooyoung <souljit2@gmail.com>
2019-11-12 01:33:15 +0900

    Software Engineering : Main Process frame

--- SoftwareEngineering/digitalWatch.c
+++ SoftwareEngineering/digitalWatch.c
@@ -1,124 +1,190 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <string.h>
-#define TRUE 1==1
-#define FALSE 1!=1
+#define TRUE 1 == 1
+#define FALSE 1 != 1
 #define ON 1
 #define OFF 0
 //struct State Data variable mode preset
 #define TIME_KEEPING_MODE 1 // mode == timeKeeping
-#define STOP_WATCH_MODE 2 // mode == stopWatch
-#define ALARM_MODE 3 // mode == alarm
+#define STOP_WATCH_MODE 2   // mode == stopWatch
+#define ALARM_MODE 3        // mode == alarm
 
 //struct State Data variable innerMode preset
-#define DEFAULT 1 // inner mode == default
-#define TIME_KEEPING_SET 2 // inner mode == timeKeeping->timeKeepingSet
-#define STOP_WATCH_RUN 3 // stopWatch mode == stopWatch->stopWatchRun
+#define DEFAULT 1            // inner mode == default
+#define TIME_KEEPING_SET 2   // inner mode == timeKeeping->timeKeepingSet
+#define STOP_WATCH_RUN 3     // stopWatch mode == stopWatch->stopWatchRun
 #define STOP_WATCH_LAPTIME 4 // stopWatch mode == stopWatch->laptime
-#define STOP_WATCH_PAUSE 5 // stopWatch mode == stopWatch->pause
+#define STOP_WATCH_PAUSE 5   // stopWatch mode == stopWatch->pause
 
 //timeKeepingUnit preset
+#define SEC 0
 #define HOUR 1
 #define MIN 2
-#define SEC 3
-#define MILLI_SEC 4
+#define YEAR 3
+#define MONTH 4
+#define DAY 5
 
-typedef struct AlarmData{
-    int indicator;
+#define INPUT_BUTTON_SIZE 100
+#define PROCESSED_BUTTON_SIZE 5
+typedef struct AlarmData
+{
+    int alarmIndicator;
+    int alarmState;
     int hour;
     int min;
-}AlarmData;
+} AlarmData;
 
-typedef struct StateData{
+typedef struct StateData
+{
     char mode;
     char innerMode;
     char timeKeepingUnit;
     char alarmUnit;
-}StateData;
+} StateData;
 
-void* thread1(void*);
-void* thread2(void*);
-void timeKeepingMode(StateData*);
-void timeKeepingSet(StateData*);
-void timeKeepingUnitChange(StateData*);
-void stopWatchMode(StateData*);
-void alarmMode(StateData*);
-void showWatch(StateData*);
+void decideMainProcess(StateData *, AlarmData *, char); //Decide main controller process based on state, button and alarm data
+void buttonInitialize(char *, int, int);                //Initialize button in range begin ~ MAX
+void *alarmThread(void *);                              //Alarm controller thread
+void *backlightThread(void *);                          //Backlight controller thread
+void buttonProcess(char *, char *);                     //Process simultaneous input processing
+void timeKeepingMode(StateData *);                      // Enable timeKeepingMode
+void timeKeepingSet(StateData *);                       // Enable timeKeepingSet
+void timeKeepingUnitChange(StateData *);                // Trigger timeKeeping unit change
+void stopWatchMode(StateData *);                        // Enable stopWatchMode
+void alarmMode(StateData *);                            // Enable alarmMode
+void showWatch(StateData *);                            // show watch based on StateData
+
 int main(void)
-{ 
-    char* weekOfDays[7] = 
-    {
-        "MO",
-        "TU",
-        "TH",
-        "WE",
-        "FR",
-        "SA",
-        "SU",
-    };
-    char button;
+{
+    char *weekOfDays[7] =
+        {
+            "MO",
+            "TU",
+            "TH",
+            "WE",
+            "FR",
+            "SA",
+            "SU",
+        };
+    char inputButton[INPUT_BUTTON_SIZE] = {0};
+    char processedButton[4];
+    StateData state;
+    AlarmData alarm;
     pthread_t Alarm;
     pthread_t BackLight;
-    // pthread_create(&Alarm, NULL, thread1, void*);
-    // pthread_create(&BackLight, NULL, thread2, void*);
-    pthread_join(&Alarm, NULL);
-    pthread_join(&BackLight, NULL);
-    // pthread_create()
+    int i;
+    // pthread_create(&Alarm, NULL, alarmThread, void*);
+    // pthread_create(&BackLight, NULL, backlightThread, void*);
+    while (1)
+    {
+        printf("Button : ");
+        scanf("%s", inputButton);
+        buttonProcess(inputButton, processedButton);
+        for (i = 0; i < INPUT_BUTTON_SIZE; i++)
+        {
+            if (processedButton[i] == 0 || !(processedButton[i] >= 'A' && processedButton[i] <= 'D'))
+                continue;
+            decideMainProcess(&state, &alarm, processedButton[i]);
+        }
+        buttonInitialize(inputButton, 0, INPUT_BUTTON_SIZE);
+        buttonInitialize(processedButton, 0, PROCESSED_BUTTON_SIZE);
+    }
+    // pthread_join(&Alarm, NULL);
+    // pthread_join(&BackLight, NULL);
     return 0;
 }
 
-void* thread1(void* test)
+void *alarmThread(void *test) //Alarm controller thread
 {
-
 }
 
-void* thread2(void* test)
+void *backlightThread(void *test) //Backlight controller thread
 {
-
 }
-
-void timeKeepingMode(StateData* state) // enable timeKeepingMode
+void decideMainProcess(StateData *state, AlarmData *alarm, char button) //Decide main controller process based on state, button and alarm data
+{
+    printf("%c\n", button);
+}
+void buttonInitialize(char *button, int begin, int size) //Initialize button in range begin ~ MAX
+{
+    int i;
+    for (i = begin; i < size; i++)
+    {
+        button[i] = 0;
+    }
+}
+void buttonProcess(char *inputButton, char *processedButton) //Process simultaneous input processing
+{
+    int i;
+    int checkButton = 0;
+    for (i = 0; i < INPUT_BUTTON_SIZE; i++)
+    {
+        if (inputButton[i] == 'A' || inputButton[i] == 'a' && processedButton[3]!='A')
+        {
+            processedButton[3] = 'A';
+            checkButton++;
+        }
+        else if (inputButton[i] == 'B' || inputButton[i] == 'b' && processedButton[2]!='B')
+        {    
+            processedButton[2] = 'B';
+            checkButton++;
+        }
+        else if (inputButton[i] == 'C' || inputButton[i] == 'c' && processedButton[1]!='C')
+        {    
+            processedButton[1] = 'C';
+            checkButton++;
+        }
+        else if (inputButton[i] == 'D' || inputButton[i] == 'd' && processedButton[0]!='D')
+        {
+            processedButton[0] = 'D';
+            checkButton++;
+        }    
+        if(checkButton==4)
+            break;
+    }
+    buttonInitialize(inputButton, 0, INPUT_BUTTON_SIZE);
+}
+void timeKeepingMode(StateData *state) // Enable timeKeepingMode
 {
     state->mode = TIME_KEEPING_MODE;
     state->innerMode = DEFAULT;
 }
 
-void timeKeepingSet(StateData* state) // enable timeKeepingSet
+void timeKeepingSet(StateData *state) // Enable timeKeepingSet
 {
-    if(state->mode == TIME_KEEPING_MODE)
+    if (state->mode == TIME_KEEPING_MODE)
         state->innerMode = TIME_KEEPING_SET;
 }
 
-void timeKeepingUnitChange(StateData* state)
+void timeKeepingUnitChange(StateData *state) // Trigger timeKeeping unit change
 {
-    if(state->mode!=TIME_KEEPING_MODE)
+    if (state->mode != TIME_KEEPING_MODE)
         return;
-    // state->timeKeepingUnit % 
+    // state->timeKeepingUnit %
 }
-void stopWatchMode(StateData* state)
+void stopWatchMode(StateData *state) // Enable stopWatchMode
 {
     state->mode = STOP_WATCH_MODE;
     state->innerMode = DEFAULT;
 }
 
-void alarmMode(StateData* state)
+void alarmMode(StateData *state) // Enable alarmMode
 {
     state->mode = ALARM_MODE;
     state->innerMode = DEFAULT;
 }
 
-void showWatch(StateData* state) // show watch based on StateData
+void showWatch(StateData *state) // show watch based on StateData
 {
-    if(state->mode == TIME_KEEPING_MODE) // Time Keeping Mode
+    if (state->mode == TIME_KEEPING_MODE) // Time Keeping Mode
     {
         printf("%02d:");
     }
-    else if(state->mode == STOP_WATCH_MODE) // Stop Watch Mode
+    else if (state->mode == STOP_WATCH_MODE) // Stop Watch Mode
     {
-
     }
     else // Alarm Mode
     {
-
     }
 }
