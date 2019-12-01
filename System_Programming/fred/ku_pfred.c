5d90bfc99a89df52fadda278ef29e4681cac84ba
Wooyoung <souljit2@gmail.com>
2019-12-01 17:00:59 +0900

    System Programming : ku_pfed message queue send finished

--- System_Programming/fred/ku_pfred.c
+++ System_Programming/fred/ku_pfred.c
@@ -1,26 +1,34 @@
 #include <stdio.h>
-#include <windows.h>
+#include <string.h>
 #include <stdlib.h>
+#include <windows.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <mqueue.h>
+
 #define MAX_SIZE 9999
+#define MSG_SIZE 8
+#define DEFAULT_NAME "/m_queue"
+// #define MAX_PRIO 32
 
 int getFirstLine(int, int *);
-int getLineByLineNum(int, int, int);
+int getValueByLineNum(int, int, int);
 void recordIntervalArray(int *, int, int, int);
-int decideInterval(int, int);
+int decideArrayIndex(int, int);
 int combineArray();
+void printIntervalArray(int *, int, int);
+void sendMsg(int, int *, int, int);
 int main(int argc, char *argv[])
 {
     int numOfProcess;
     int interval;
     int processNum;
     int *intervalArray;
-    int arraySize;
-    int numOfLine;
+    int arrayLength;
+    int maxLineNum;
     int i;
     int fd;
     int processSize;
@@ -34,17 +42,19 @@ int main(int argc, char *argv[])
     }
     numOfProcess = atoi(argv[1]);
     interval = atoi(argv[2]);
-    arraySize = MAX_SIZE / interval + 1;
-    intervalArray = (int *)malloc(sizeof(int) * arraySize);
-    // if ((fd = open(fileName, O_RDONLY)) < 0)
+    arrayLength = MAX_SIZE / interval + 1;
+    intervalArray = (int *)malloc(sizeof(int) * arrayLength);
+    if ((fd = open(fileName, O_RDONLY)) < 0)
+    {
+        printf("file open failed\n");
+        exit(1);
+    }
+    getFirstLine(fd, &maxLineNum);
+    close(fd);
+    // getFirstLine(fd, &maxLineNum);
+    // for(i=1;i<=maxLineNum;i++)
     // {
-    //     printf("file open failed\n");
-    //     exit(1);
-    // }
-    // getFirstLine(fd, &numOfLine);
-    // for(i=1;i<=numOfLine;i++)
-    // {
-    //     printf("Line %d : %d\n",i,getLineByLineNum(fd,i, numOfLine));
+    //     printf("Line %d : %d\n",i,getValueByLineNum(fd,i, numOfLine));
     // }
     for (processNum = 0; processNum < numOfProcess; processNum++)
         if ((pid = fork()) == 0)
@@ -52,26 +62,28 @@ int main(int argc, char *argv[])
         else if (pid < 0)
         {
             perror("fork error\n");
-            processNum--;
+            return 1;
         }
 
     // printf("%d\n", fd);
     if (pid == 0)
     {
         intervalArray[0] = interval;
-        recordIntervalArray(intervalArray, arraySize, processNum, numOfProcess);
-        // for(i=0;i<arraySize;i++)
+        recordIntervalArray(intervalArray, arrayLength, processNum, numOfProcess);
+        // for(i=0;i<arrayLength;i++)
         //     printf("%d\n",intervalArray[i]);
     }
-    free(intervalArray);
-    if (pid != 0)
+    else
+    {
         for (i = 0; i < numOfProcess; i++)
-        {
-            // if (combineArray() == -1)
-            //     printf("message queue error\n");
             wait(NULL);
-        }
-
+        numOfProcess = numOfProcess < maxLineNum ? numOfProcess : maxLineNum;
+        // printf("%d\n", numOfProcess);
+        // for (i = 0; i < numOfProcess; i++)
+        //     sendMsg(processNum, intervalArray, arrayLength, numOfProcess);
+    }
+    combineArray();
+    free(intervalArray);
     return 0;
 }
 int getFirstLine(int fd, int *numOfLine)
@@ -92,7 +104,7 @@ int getFirstLine(int fd, int *numOfLine)
     return offsetBytes;
 }
 
-int getLineByLineNum(int fd, int lineNum, int maxLineNum)
+int getValueByLineNum(int fd, int lineNum, int maxLineNum)
 {
     char *tmp;
     int tmpInt;
@@ -108,14 +120,14 @@ int getLineByLineNum(int fd, int lineNum, int maxLineNum)
     return tmpInt;
 }
 
-void recordIntervalArray(int *intervalArray, int arraySize, int processNum, int numOfProcess)
+void recordIntervalArray(int *intervalArray, int arrayLength, int processNum, int numOfProcess)
 {
     int i;
     int lineNum;
     int offset;
     int processSize;
     int maxLineNum;
-    int interval = intervalArray[0];
+    const int interval = intervalArray[0];
     char tmp[20];
     int fd;
     fd = open("dataset", O_RDONLY);
@@ -129,24 +141,51 @@ void recordIntervalArray(int *intervalArray, int arraySize, int processNum, int 
         processSize = offset + (maxLineNum % numOfProcess);
     else //Other processes process file values ​​by the given offset.
         processSize = offset;
-    if (processNum + 1 <= maxLineNum) // if the number of processes is over maxLineNum, 1~maxLineNum processes process file and other processes(maxLineNum+1~) do nothing.
+    if (processNum < maxLineNum) // if the number of processes is over maxLineNum, 1~maxLineNum processes process file and other processes(maxLineNum+1~) do nothing.
     {
-        for (i = 0; i < arraySize; i++) //initialize intervalArray
+        for (i = 0; i < arrayLength; i++) //initialize intervalArray
             intervalArray[i] = 0;
         // printf("processNum : %d, processSize : %d\n",processNum,processSize);
         for (i = 0; i < processSize; i++)
-        {
-            printf("processNum : %d, line %d : %d(arrayIdx=%d)\n", processNum + 1, offset * processNum + i + 1, getLineByLineNum(fd, offset * processNum + i + 1, maxLineNum), decideInterval(getLineByLineNum(fd, offset * processNum + i + 1, maxLineNum), interval));
-            // intervalArray[decideInterval(getLineByLineNum(fd, offset * processNum + i + 1, maxLineNum), interval)]++;
-        }
+            intervalArray[decideArrayIndex(getValueByLineNum(fd, offset * processNum + i + 1, maxLineNum), interval)]++;
+        sendMsg(processNum, intervalArray, arrayLength, numOfProcess);
+        // printIntervalArray(intervalArray, arrayLength, processNum);
+        // printf("processNum : %d, line %d : %d(arrayIdx=%d)\n", processNum + 1, offset * processNum + i + 1, getValueByLineNum(fd, offset * processNum + i + 1, maxLineNum), decideArrayIndex(getValueByLineNum(fd, offset * processNum + i + 1, maxLineNum), interval));
     }
 }
-int decideInterval(int num, int interval)
+void printIntervalArray(int *intervalArray, int arrayLength, int processNum)
+{
+    int i;
+    for (i = 0; i < arrayLength; i++)
+    {
+        printf("process %d(size %d, idx %d) : %d\n", processNum, arrayLength, i, intervalArray[i]);
+    }
+}
+int decideArrayIndex(int num, int interval)
 {
     return num / interval;
 }
 
+void sendMsg(int processNum, int *intervalArray, int arrayLength, int numOfProcess)
+{
+    struct mq_attr attr;
+    unsigned int prio = 0;
+    mqd_t mqdes;
+    attr.mq_maxmsg = numOfProcess;
+    attr.mq_msgsize = arrayLength * 4;
+    if ((mqdes = mq_open(DEFAULT_NAME, O_CREAT | O_WRONLY, 0666, &attr)) < 0)
+    {
+        perror("mq_open()");
+        exit(0);
+    }
+    // printf("Sending a message %d with priority %d\n", intervalArray, prio);
+    if (mq_send(mqdes, (char *)intervalArray, attr.mq_msgsize, prio) == -1)
+        perror("mq_send()");
+
+    mq_close(mqdes);
+    printf("process %d send finished\n", processNum);
+}
 int combineArray()
 {
-    return -1;
+    // printf("1\n");
 }
