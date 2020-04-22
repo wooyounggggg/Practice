f774f3cf5100cbf1e2176de5680ac2b29eace977
wooyoung <souljit2@gmail.com>
2020-04-22 16:41:40 +0900

    modify Network-Programming permission

--- Network-Programming/server.c
+++ Network-Programming/server.c
@@ -6,23 +6,20 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #define BUFF_SIZE 1024
-
+int i = 0;
 int main(void)
 {
+    struct sockaddr_in server_addr;
+    struct sockaddr_in client_addr;
     int server_socket = 0; /* for server socket fd allocation */
     int client_socket = 0; /* for client socket fd allocation */
     int client_addr_size = sizeof(client_addr);
 
-    struct sockaddr_in server_addr;
-    struct sockaddr_in client_addr;
-
     char buff[BUFF_SIZE + 5]; /*  */
     char buff_rcv[BUFF_SIZE];
     char buff_snd[BUFF_SIZE];
-
     memset(&server_addr, 0x00, sizeof(server_addr));
     memset(&client_addr, 0x00, sizeof(client_addr));
-
     server_socket = socket(PF_INET, SOCK_STREAM, 0);
     if (server_socket == -1)
     {
@@ -40,7 +37,7 @@ int main(void)
     if (listen(server_socket, 5) == -1)
     {
         printf("listen error\n");
-        exit(1)
+        exit(1);
     }
     client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_addr_size);
     if (client_socket == -1)
@@ -48,6 +45,19 @@ int main(void)
         printf("client connection error\n");
         exit(1);
     }
-
+    while (1)
+    {
+        client_addr_size = sizeof(client_addr);
+        client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_addr_size);
+        if (-1 == client_socket)
+        {
+            printf("클라이언트 연결수락실패\n");
+            exit(1);
+        }
+        read(client_socket, buff_rcv, BUFF_SIZE);
+        printf("receive: %s\n", buff_rcv);
+        sprintf(buff_snd, "%d : %s", strlen(buff_rcv), buff_rcv);
+        write(client_socket, buff_snd, strlen(buff_snd) + 1); // +1: NULL까지 포함해서 전송 close( client_socket);
+    }
     return 0;
 }
