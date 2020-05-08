0187488004a425e4b8f6c544db8fd133bf4b71bd
wooyoung <souljit2@gmail.com>
2020-05-09 01:30:53 +0900

    OS : add Forward Declaration

--- OS/ku_mmu/ku_mmu.h
+++ OS/ku_mmu/ku_mmu.h
@@ -25,59 +25,57 @@ PCB *PCBList = NULL;
 struct ku_pte *pmem = NULL;
 struct ku_pte *swapSpace = NULL;
 
-PCB *addPCBElement(char pid) /* add Process' PCB element and map PDBR to pmem */
+PCB *addPCBElement(char);
+PCB *getPCBByPid(char);
+int getPTEState(struct ku_pte *);
+char *getPageIndexesByVA(char);
+int mapPageOrTable(struct ku_pte *, int);
+int swapPageOrTable(struct ku_pte *);
+struct ku_pte *getPageTableByPFN(char);
+char getPFNByEntry(char);
+int mappingProcess(struct ku_pte *, char);
+int ku_page_fault(char, char);
+void *ku_mmu_init(unsigned int, unsigned int);
+int ku_run_proc(char, struct ku_pte **);
+
+/* handle page fault by 'demand page' or 'swapping(FIFO)'. Page Directory and Page Table not swapped out */
+int ku_page_fault(char pid, char va)
 {
-    PCB *newPCB = PCBList;
-    while (newPCB != NULL)
-        newPCB = newPCB->next;
-    newPCB = (PCB *)malloc(sizeof(PCB));
-    if (newPCB == NULL)
-        return NULL;
-    newPCB->PDBR = (struct ku_pte *)malloc(sizeof(struct ku_pte) * 4);
-    if (newPCB->PDBR == NULL)
+    /* 1. get PCB by PID */
+    PCB *PCBByPid = getPCBByPid(pid);
+    /* 2. Page Directory ~ Page Mapping*/
+    mappingProcess(PCBByPid->PDBR, va);
+    /* 3. */
+    if (1 == 1) /* if success, return 0 */
+        return 0;
+    return -1; /* else return -1 */
+}
+void *ku_mmu_init(unsigned int mem_size, unsigned int swap_size) /* initialize resource. called only once in starting. */
+{
+    /*1. Allocate memory space for pmem */
+    pmem = (struct ku_pte *)malloc(sizeof(struct ku_pte) * mem_size);
+    // struct ku_pte *pmem = (struct ku_pte *)malloc(sizeof(struct ku_pte) * mem_size);
+    if (pmem == NULL)
+        return 0;
+    /*2. Allocate swap space*/
+    swapSpace = (struct ku_pte *)malloc(sizeof(struct ku_pte) * swap_size);
+    if (swapSpace == NULL)
     {
-        free(newPCB);
-        return NULL;
+        free(pmem);
+        return 0;
     }
-    mapPageOrTable(newPCB->PDBR, TABLE);
-    newPCB->prev = NULL;
-    newPCB->next = NULL;
-    newPCB->pid = pid;
-    return newPCB;
+    return pmem;
 }
-PCB *getPCBByPid(char pid)
+int ku_run_proc(char pid, struct ku_pte **ku_cr3) /* Performs Context Switch. If pid is new, function creates a process in virtual and its page directory */
 {
-    PCB *tmp = PCBList;
-    while (tmp != NULL)
-    {
-        if (tmp->pid == pid)
-            return tmp;
-        tmp = tmp->next;
-    }
-    return NULL;
-}
-int getPTEState(struct ku_pte *PTE)
-{
-    if (PTE->entry == 0)
-        return INVALID;
-    else if ((PTE->entry & 0x01) && (PTE->entry >> 2)) /* if Present bit is 1 and PFN > 0, return PRESENT */
-        return PRESENT;
-    else if (!(PTE->entry & 0x01) && (PTE->entry >> 1)) /* if Present bit is 0 and Swap Offset > 0, return SWAPPED */
-        return SWAPPED;
-    else if (PTE->entry & 0x02)
-        return PAGE;
-}
-char *getPageIndexesByVA(char va)
-{
-    char *pageIndexes;
-    pageIndexes = (char *)malloc(sizeof(char) * 4);
-    if (pageIndexes == NULL)
-        return NULL;
-    pageIndexes[PDE_INDEX] = va >> 6;
-    pageIndexes[PMDE_INDEX] = va >> 4 & 0x03;
-    pageIndexes[PTE_INDEX] = va >> 2 & 0x03;
-    pageIndexes[PAGE_INDEX] = va & 0x03;
-    return pageIndexes;
+    /* 1. get PCB(global) which has parameter local variable pid(functionally) : getPCBByPid(char pid) */
+    PCB *PCBByPid = getPCBByPid(pid);
+    if (PCBByPid == NULL)
+        if ((PCBByPid = addPCBElement(pid)) == NULL)
+            return -1;
+    /* 2. map ku_cr3 to PCB's PDBR */
+    *ku_cr3 = PCBByPid->PDBR;
+    return 0;
 }
 
 /* Page Directory와 Page Middle Dir, Page Table 및 Page를 pmem에 적재해야 하는데, Page Directory는 PCB->PDBR로 적재를 하지 va를 통한 indexing으로 적재하지 않기 때문에,
@@ -133,42 +131,57 @@ int mappingProcess(struct ku_pte *pageDirectory, char va) /* map Page Directory 
     free(pageIndexes);
 }
 
-/* handle page fault by 'demand page' or 'swapping(FIFO)'. Page Directory and Page Table not swapped out */
-int ku_page_fault(char pid, char va)
+PCB *addPCBElement(char pid) /* add Process' PCB element and map PDBR to pmem */
 {
-    /* 1. get PCB by PID */
-    PCB *PCBByPid = getPCBByPid(pid);
-    /* 2. Page Directory ~ Page Mapping*/
-    mappingProcess(PCBByPid->PDBR, va);
-    /* 3. */
-    if (1 == 1) /* if success, return 0 */
-        return 0;
-    return -1; /* else return -1 */
-}
-void *ku_mmu_init(unsigned int mem_size, unsigned int swap_size) /* initialize resource. called only once in starting. */
-{
-    /*1. Allocate memory space for pmem */
-    pmem = (struct ku_pte *)malloc(sizeof(struct ku_pte) * mem_size);
-    // struct ku_pte *pmem = (struct ku_pte *)malloc(sizeof(struct ku_pte) * mem_size);
-    if (pmem == NULL)
-        return 0;
-    /*2. Allocate swap space*/
-    swapSpace = (struct ku_pte *)malloc(sizeof(struct ku_pte) * swap_size);
-    if (swapSpace == NULL)
+    PCB *newPCB = PCBList;
+    while (newPCB != NULL)
+        newPCB = newPCB->next;
+    newPCB = (PCB *)malloc(sizeof(PCB));
+    if (newPCB == NULL)
+        return NULL;
+    newPCB->PDBR = (struct ku_pte *)malloc(sizeof(struct ku_pte) * 4);
+    if (newPCB->PDBR == NULL)
     {
-        free(pmem);
-        return 0;
+        free(newPCB);
+        return NULL;
     }
-    return pmem;
+    mapPageOrTable(newPCB->PDBR, TABLE);
+    newPCB->prev = NULL;
+    newPCB->next = NULL;
+    newPCB->pid = pid;
+    return newPCB;
 }
-int ku_run_proc(char pid, struct ku_pte **ku_cr3) /* Performs Context Switch. If pid is new, function creates a process in virtual and its page directory */
+PCB *getPCBByPid(char pid)
 {
-    /* 1. get PCB(global) which has parameter local variable pid(functionally) : getPCBByPid(char pid) */
-    PCB *PCBByPid = getPCBByPid(pid);
-    if (PCBByPid == NULL)
-        if ((PCBByPid = addPCBElement(pid)) == NULL)
-            return -1;
-    /* 2. map ku_cr3 to PCB's PDBR */
-    *ku_cr3 = PCBByPid->PDBR;
-    return 0;
+    PCB *tmp = PCBList;
+    while (tmp != NULL)
+    {
+        if (tmp->pid == pid)
+            return tmp;
+        tmp = tmp->next;
+    }
+    return NULL;
+}
+int getPTEState(struct ku_pte *PTE)
+{
+    if (PTE->entry == 0)
+        return INVALID;
+    else if ((PTE->entry & 0x01) && (PTE->entry >> 2)) /* if Present bit is 1 and PFN > 0, return PRESENT */
+        return PRESENT;
+    else if (!(PTE->entry & 0x01) && (PTE->entry >> 1)) /* if Present bit is 0 and Swap Offset > 0, return SWAPPED */
+        return SWAPPED;
+    else if (PTE->entry & 0x02)
+        return PAGE;
+}
+char *getPageIndexesByVA(char va)
+{
+    char *pageIndexes;
+    pageIndexes = (char *)malloc(sizeof(char) * 4);
+    if (pageIndexes == NULL)
+        return NULL;
+    pageIndexes[PDE_INDEX] = va >> 6;
+    pageIndexes[PMDE_INDEX] = va >> 4 & 0x03;
+    pageIndexes[PTE_INDEX] = va >> 2 & 0x03;
+    pageIndexes[PAGE_INDEX] = va & 0x03;
+    return pageIndexes;
 }
