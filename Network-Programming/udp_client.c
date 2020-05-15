95a1a80ba4ef2d4606a62e968871675dccfdb3b5
wooyoung <souljit2@gmail.com>
2020-05-16 03:39:37 +0900

    making udp server & udp client

--- Network-Programming/udp_client.c
+++ Network-Programming/udp_client.c
@@ -7,7 +7,12 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #define BUFF_SIZE 1024
-
+typedef struct Packet
+{
+    char data[4096];
+    int data_size;
+    int data_seq;
+} Packet;
 int main(void)
 {
     int client_socket;
--- Network-Programming/udp_server.c
+++ Network-Programming/udp_server.c
@@ -7,6 +7,12 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #define BUFF_SIZE 1024
+typedef struct Packet
+{
+    char data[4096];
+    int data_size;
+    int data_seq;
+} Packet;
 int main(void)
 {
     int serverSocket;
@@ -14,9 +20,9 @@ int main(void)
     struct sockaddr_in client_addr;
     int bindState;
     char buf[BUFF_SIZE];
-    serveraddr.sin_family = AF_INET;
-    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serveraddr.sin_port = htons(4000);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(4000);
     serverSocket = socket(AF_INET, SOCK_DGRAM, 0);
     int client_addr_size = sizeof(client_addr);
     memset(&server_addr, 0, sizeof(server_addr));
@@ -32,6 +38,8 @@ int main(void)
         exit(1);
     }
     recvfrom(serverSocket, buf, BUFF_SIZE, 0, (struct sockaddr *)&client_addr, &client_addr_size);
-
+    printf("receive : %s\n", buf);
+    sendto(serverSocket, buf, strlen(buf), 0, (struct sockaddr *)&client_addr, sizeof(client_addr));
+    close(serverSocket);
     return 0;
 }
