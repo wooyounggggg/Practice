87c870f89976ef340cdc6b509fe99e249875a538
wooyoung <souljit2@gmail.com>
2020-05-16 04:26:21 +0900

    modify udp client

--- Network-Programming/udp_client.c
+++ Network-Programming/udp_client.c
@@ -5,17 +5,28 @@
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <fcntl.h>
+#define BUFF_SIZE 4096
 
-#define BUFF_SIZE 1024
-
+typedef struct Packet
+{
+    char data[4096];
+    int data_size;
+    int data_seq;
+} Packet;
 int main(int argc, char **argv)
 {
-    int sock;
+    int rd_size;
+    int fd;
+    int serverSocket;
     int server_addr_size;
     struct sockaddr_in server_addr;
-    char buff_rcv[BUFF_SIZE + 5];
-    sock = socket(AF_INET, SOCK_DGRAM, 0);
-    if (-1 == sock)
+    Packet packet;
+    packet.data_seq = 1;
+    char buff_rcv[BUFF_SIZE + 1];
+    char buff_send[BUFF_SIZE + 1];
+    serverSocket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (-1 == serverSocket)
     {
         printf("socket 생성 실패n");
         exit(1);
@@ -24,15 +35,27 @@ int main(int argc, char **argv)
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(4000);
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    if (0 < (fd = open("./input.txt", O_RDONLY)))
+    {
+        while (0 < (packet.data_size = read(fd, packet.data, BUFF_SIZE))) //  4 byte씩
+        {
+            packet.data[packet.data_size] = '\0';
+            sendto(serverSocket, packet.data, strlen(packet.data) + 1, 0, // +1: NULL까지 포함해서 전송
+                   (struct sockaddr *)&server_addr, sizeof(server_addr));
+            printf("%d bytes data(seq %d) send\n", packet.data_size, packet.data_seq);
+            packet.data_seq++;
+        }
+        close(fd);
+    }
+    else
+    {
+        printf("파일 열기에 실패했습니다.\n");
+        return 0;
+    }
 
-    sendto(sock, argv[1], strlen(argv[1]) + 1, 0, // +1: NULL까지 포함해서 전송
-           (struct sockaddr *)&server_addr, sizeof(server_addr));
-
-    server_addr_size = sizeof(server_addr);
-    recvfrom(sock, buff_rcv, BUFF_SIZE, 0,
-             (struct sockaddr *)&server_addr, &server_addr_size);
-    printf("receive: %sn", buff_rcv);
-    close(sock);
+    /* sendto(serverSocket, "0x1A", strlen("0x1A") + 1, 0, // +1: NULL까지 포함해서 전송
+           (struct sockaddr *)&server_addr, sizeof(server_addr)); */
+    close(serverSocket);
 
     return 0;
 }
--- Network-Programming/udp_server_2.c
+++ Network-Programming/udp_server_2.c
@@ -6,22 +6,23 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 
-#define BUFF_SIZE 1024
+#define BUFF_SIZE 4096
 
 int main(void)
 {
-    int sock;
+    int seq = 1;
+    int serverSocket;
     int client_addr_size;
 
     struct sockaddr_in server_addr;
     struct sockaddr_in client_addr;
 
-    char buff_rcv[BUFF_SIZE + 5];
-    char buff_snd[BUFF_SIZE + 5];
+    char buff_rcv[BUFF_SIZE];
+    char buff_snd[BUFF_SIZE];
 
-    sock = socket(PF_INET, SOCK_DGRAM, 0);
+    serverSocket = socket(PF_INET, SOCK_DGRAM, 0);
 
-    if (-1 == sock)
+    if (-1 == serverSocket)
     {
         printf("socket 생성 실패n");
         exit(1);
@@ -32,21 +33,24 @@ int main(void)
     server_addr.sin_port = htons(4000);
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    if (-1 == bind(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)))
+    if (-1 == bind(serverSocket, (struct sockaddr *)&server_addr, sizeof(server_addr)))
     {
         printf("bind() 실행 에러n");
         exit(1);
     }
 
-    while (1)
+    while (strcmp(buff_rcv, "0x1A") != 0)
     {
+        printf("test\n");
         client_addr_size = sizeof(client_addr);
-        recvfrom(sock, buff_rcv, BUFF_SIZE, 0,
+        recvfrom(serverSocket, buff_rcv, BUFF_SIZE, 0,
                  (struct sockaddr *)&client_addr, &client_addr_size);
-        printf("receive: %sn", buff_rcv);
+        printf("%d byte data (seq %d) received.", client_addr_size, seq);
 
-        sprintf(buff_snd, "%s%s", buff_rcv, buff_rcv);
-        sendto(sock, buff_snd, strlen(buff_snd) + 1, 0, // +1: NULL까지 포함해서 전송
+        sprintf(buff_snd, "%s", buff_rcv, buff_rcv);
+        sendto(serverSocket, buff_snd, strlen(buff_snd) + 1, 0, // +1: NULL까지 포함해서 전송
                (struct sockaddr *)&client_addr, sizeof(client_addr));
+        seq++;
     }
+    close(serverSocket);
 }
